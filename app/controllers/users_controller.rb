e048289e63fa8a02cf51097182b3d05f147b7bf7
Anton <saneseo@gmail.com>
2013-08-15 13:05:55 +0400

    Finish the signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def new
+		@user = User.new
   end
+	
+	def show
+		#raise params.inspect
+		@user = User.find(params[:id])
+	end
+	
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
+	resources :users
+
   root :to => 'static_pages#home'
 	
-	match 'signup', to: 'users#new'
+	match '/signup', to: 'users#new'
 	
 	match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
